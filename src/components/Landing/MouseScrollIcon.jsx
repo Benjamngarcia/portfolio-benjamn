c46c41f03ee1c5b618912201d24404d3951c865f
Benjamngarcia <bperezg1800@alumno.ipn.mx>
2023-04-25 17:00:15 -0600

    create prod static files

--- src/components/Landing/MouseScrollIcon.jsx
+++
@@ -1,9 +0,0 @@
-export const MouseScroll = () => {
-  return (
-    <div className="scroll-downs">
-      <div className="mousey">
-        <div className="scroller"></div>
-      </div>
-    </div>
-  )
-}
